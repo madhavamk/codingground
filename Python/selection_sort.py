fcda14aeb8fc994aabc5dbebc5c8fb50fa710f78
madhavamk <madhava.mk.123@gmail.com>
2024-02-05 22:43:08 +0530

    Create selection_sort.py

---
+++ Python/selection_sort.py
@@ -0,0 +1,13 @@
+def selection_sort(a):
+    n = len(a)
+    for i in range(n - 1):
+        small = i
+        for j in range(i+1, n):
+            if a[j] < a[small]:
+                small = j
+        a[small],a[i] = a[i],a[small]
+
+print("************Implementation of BubbleSort***********")
+a = [23,421,11,-11,423]
+selection_sort(a)
+print(a)
