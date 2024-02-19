e327014ac7eaa75d2eba45b8444d2b3da9d416a1
madhavamk <madhava.mk.123@gmail.com>
2024-02-19 21:23:43 +0530

    Update quicksort.py

--- Python/quicksort.py
+++ Python/quicksort.py
@@ -1,21 +1,22 @@
 def partition(a,l,r):
     p = a[l]
-    i = l
-    j = r
-    while i <= j:
-        while a[i] <= p:
+    i = l-1
+    j = r+1
+    while True:
+        i+=1
+        while a[i] < p:
             i+=1
+        j-=1
         while a[j] > p:
             j-=1
+        if i>=j:
+            return j
         a[i],a[j] = a[j],a[i]
-    a[i],a[j] = a[j],a[i]
-    a[l],a[j] = a[j],a[l]
-    return j
 
 def quick_sort(a,l,r):
     if l < r:
         s = partition(a,l,r)
-        quick_sort(a,l,s-1)
+        quick_sort(a,l,s)
         quick_sort(a,s+1,r)
 
 print("************Implementation of QuickSort***********")
